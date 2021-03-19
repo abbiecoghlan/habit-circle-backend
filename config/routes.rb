e47affe5348f3e982986618fc229777ca18df386
Abbie <abbie.coghlan@gmail.com>
2021-03-19 12:31:42 -0500

    update serializers, added route for user progress

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+  post "/user_info", to: 'progresses#user_info'
+
+  # post '/current_month', to: 'users#current_month'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
