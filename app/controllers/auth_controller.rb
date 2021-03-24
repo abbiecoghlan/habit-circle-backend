98fc3a0bdaf77334ab750652e7d429edc4fd9f48
“Abbie <abbie.coghlan@gmail.com>
2021-03-24 11:40:51 -0500

    create user, jwt working

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,14 +6,17 @@ class AuthController < ApplicationController
 
         #CHECK IF THE USER IS VALID
         if @user && @user.authenticate(user_login_params[:password])
-            #CHECK IF THE USER HAS PROGRESS FOR THEIR HABITS FOR THIS MONTH- IF THEY DONT, MAKE THEM
-            if @user.progresses.any?{|p| p.day.month == Time.now.month && p.day.year == Time.now.year} == false
-                Day.all.select { |day| day.month == Time.now.month && day.year == Time.now.year}.each { |day| 
-                @user.habits.each { |habit| 
-                    Progress.create(habit_id: habit.id, day_id: day.id, completed: false)
-                    }
-                }
-            end               
+          
+          #NOT THE PLACE TO DO THIS
+          # #CHECK IF THE USER HAS PROGRESS FOR THEIR HABITS FOR THIS MONTH- IF THEY DONT, MAKE THEM
+          #   if @user.progresses.any?{|p| p.day.month == Time.now.month && p.day.year == Time.now.year} == false
+          #       Day.all.select { |day| day.month == Time.now.month && day.year == Time.now.year}.each { |day| 
+          #       @user.habits.each { |habit| 
+          #           Progress.create(habit_id: habit.id, day_id: day.id, completed: false)
+          #           }
+          #       }
+          #   end               
+
           token = encode_token({ user_id: @user.id })
           render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
         else
