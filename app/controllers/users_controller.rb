a01c5d0852f40eb0b11355cafacaea9da58be472
“Abbie <abbie.coghlan@gmail.com>
2021-03-29 19:38:29 -0500

    updated serializers to optimize loading for better ux

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
       private
     
       def user_params
-        params.require(:user).permit(:username, :password, :name)
+        params.require(:user).permit(:username, :password, :name, :created)
       end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username
-  has_many :habits
-  has_many :progresses, through: :habits
+  attributes :id, :name, :username, :created
 
+  # has_many :habits
+  # has_many :progresses, through: :habits
+
+  
   # took progresses out of attributes
     # , :promises 
 
-  # def current_month
-  #   object.progresses.select {|p| p.day.month == Time.now.month && p.day.year == Time.now.year}
-  # end 
+  def created
+    object.created_at.month
+  end 
 end
